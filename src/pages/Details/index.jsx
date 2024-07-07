1f7f5a083151c2d1a351f4db4194046f2f165051
apaolaoliveira <paolaoliveira.dev@gmail.com>
2024-07-07 19:03:57 -0300

    add tag component

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -3,18 +3,24 @@ import { Container, Links } from './styles.js';
 import { Header } from '../../components/Header';
 import { Button } from '../../components/Button';
 import { Section } from '../../components/Section';
+import { Tag } from '../../components/Tag';
 
 export function Details(){
   return(
     <Container>
       <Header />
 
-      <Section title='Links úteis'>
+      <Section title='Resources'>
         <Links>
           <li><a href="">Link 1</a></li>
           <li><a href="">Link 2</a></li>
           <li><a href="">Link 3</a></li>
         </Links>
+      </Section>
+
+      <Section title='Tags'>
+        <Tag title="node" />
+        <Tag title="express" />
       </Section>
 
       <Button title='Back'/>
