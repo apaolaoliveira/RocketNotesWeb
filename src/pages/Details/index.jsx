ce35813ec19029c70379172a917351a61d383680
apaolaoliveira <paolaoliveira.dev@gmail.com>
2024-07-07 19:10:10 -0300

    add buttonText component

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,20 +1,23 @@
 import { Container, Links } from './styles.js';
 
 import { Header } from '../../components/Header';
-import { Button } from '../../components/Button';
 import { Section } from '../../components/Section';
 import { Tag } from '../../components/Tag';
+import { Button } from '../../components/Button';
+import { ButtonText } from '../../components/ButtonText';
 
 export function Details(){
   return(
     <Container>
       <Header />
 
+      <ButtonText title="Delete note" />
+
       <Section title='Resources'>
         <Links>
-          <li><a href="">Link 1</a></li>
-          <li><a href="">Link 2</a></li>
-          <li><a href="">Link 3</a></li>
+          <li><a href="#">Link 1</a></li>
+          <li><a href="#">Link 2</a></li>
+          <li><a href="#">Link 3</a></li>
         </Links>
       </Section>
 
