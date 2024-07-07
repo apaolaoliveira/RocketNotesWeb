b3bba80f3563f9d61a7c96fe76cbc8c952be9836
apaolaoliveira <paolaoliveira.dev@gmail.com>
2024-07-07 18:36:27 -0300

    add section component

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,11 +1,22 @@
-import { Container } from './styles.js';
+import { Container, Links } from './styles.js';
+
 import { Header } from '../../components/Header';
 import { Button } from '../../components/Button';
+import { Section } from '../../components/Section';
 
 export function Details(){
   return(
     <Container>
       <Header />
+
+      <Section title='Links úteis'>
+        <Links>
+          <li><a href="">Link 1</a></li>
+          <li><a href="">Link 2</a></li>
+          <li><a href="">Link 3</a></li>
+        </Links>
+      </Section>
+
       <Button title='Back'/>
     </Container>
   )
