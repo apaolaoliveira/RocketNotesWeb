204aebc4904160a1300ee04248160dc1ecb3963c
apaolaoliveira <paolaoliveira.dev@gmail.com>
2024-07-19 23:33:46 -0300

    ins react router, add routes

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,14 @@ import { ThemeProvider } from 'styled-components';
 import theme from './styles/theme';
 import GlobalStyles from './styles/global';
 
-import { NewNote } from './pages/NewNote';
+import { Routes } from './pages/routes';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />  
-      <NewNote />
+      <Routes />
     </ThemeProvider>
   </React.StrictMode>,
 )
