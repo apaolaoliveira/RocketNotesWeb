3a8ebb30205b24f14350bc50035765bf8b112707
apaolaoliveira <paolaoliveira.dev@gmail.com>
2024-09-07 23:20:52 -0300

    fea user signUp

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -2,11 +2,34 @@
 import { Background, Container, Form } from "./styles";
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
+import { api } from "../../services/api";
 import { FiLock, FiUser } from 'react-icons/fi';
 import { MdOutlineAlternateEmail } from "react-icons/md";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export function SignUp(){
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const navigate = useNavigate();
+
+  function handleSignUp(){
+    if(!name || !email || !password) return alert('Please fill all the required fields');
+    
+    api
+      .post('/users', { name, email, password })
+      .then(() => {
+        alert('User created successfully');
+        navigate('/');
+      })
+      .catch(err => {
+        if(err.response) alert(err.response.data.message);
+        else alert('It was not possible to sign up');
+      });
+  }
+
   return(
     <Container>
       <Background />
@@ -19,19 +42,22 @@ export function SignUp(){
           placeholder="Name"
           type="text"
           icon={FiUser}
+          onChange={e => setName(e.target.value)}
         />
         <Input 
           placeholder="Email"
           type="text"
           icon={MdOutlineAlternateEmail}
+          onChange={e => setEmail(e.target.value)}
         />
         <Input 
           placeholder="Password"
           type="password"
           icon={FiLock}
+          onChange={e => setPassword(e.target.value)}
         />
 
-        <Button title="Sign up" />
+        <Button title="Sign up" onCLick={handleSignUp}/>
 
         <Link to="/">Back</Link>
       </Form>
