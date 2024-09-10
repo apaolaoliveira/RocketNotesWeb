90472d779e62a1d4bf87ae298316a7247066ae24
apaolaoliveira <paolaoliveira.dev@gmail.com>
2024-09-09 23:57:21 -0300

    fea sign in

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -5,9 +5,17 @@ import { FiLock } from 'react-icons/fi';
 import { MdOutlineAlternateEmail } from "react-icons/md";
 import { Link } from "react-router-dom";
 import { useAuth } from '../../hooks/auth';
+import { useState } from "react";
 
 export function SignIn(){
-  const data = useAuth();
+  const { signIn } = useAuth();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleSignIn(){
+    signIn({ email, password });
+  }
 
   return(
     <Container>
@@ -20,14 +28,16 @@ export function SignIn(){
           placeholder="Email"
           type="text"
           icon={MdOutlineAlternateEmail}
+          onChange={e => setEmail(e.target.value)}
         />
         <Input 
           placeholder="Password"
           type="password"
           icon={FiLock}
+          onChange={e => setPassword(e.target.value)}
         />
 
-        <Button title="Login" />
+        <Button title="Login" onClick={handleSignIn}/>
 
         <Link to="/register">Sign up</Link>
       </Form>
