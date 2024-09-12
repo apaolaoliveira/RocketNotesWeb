499e66102ba8c242c70042aecd8194e1c8fe7025
apaolaoliveira <paolaoliveira.dev@gmail.com>
2024-09-11 23:52:30 -0300

    add user and token to local storage

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useState, useEffect } from 'react';
 import { api } from '../services/api';
 
 export const AuthContext = createContext({});
@@ -11,6 +11,9 @@ function AuthProvider({ children }) {
       const response = await api.post('/sessions', { email, password });
       const { user, token } = response.data;
 
+      localStorage.setItem('@rocketnotes:user', JSON.stringify(user));
+      localStorage.setItem('@rocketnotes:token', token);
+
       api.defaults.headers.authorization = `Bearer ${token}`;
       setData({ user, token });
     } catch (err) {
@@ -18,6 +21,16 @@ function AuthProvider({ children }) {
       else alert('It was not possible to sign in');
     }
   }
+
+  useEffect(() => {
+    const user = localStorage.getItem('@rocketnotes:user');
+    const token = localStorage.getItem('@rocketnotes:token');
+
+    if(user && token) {
+      api.defaults.headers.authorization = `Bearer ${token}`;
+      setData({ user: JSON.parse(user), token });
+    }
+  }, []);
 
   return (
     <AuthContext.Provider value={{ signIn, user: data.user }}>
