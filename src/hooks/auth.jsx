eebfc041862163c70abeb7a4bbbd9f2bad636041
apaolaoliveira <paolaoliveira.dev@gmail.com>
2024-09-13 23:57:04 -0300

    fix header request

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -14,7 +14,7 @@ export function AuthProvider({ children }) {
       localStorage.setItem('@rocketnotes:user', JSON.stringify(user));
       localStorage.setItem('@rocketnotes:token', token);
 
-      api.defaults.headers.authorization = `Bearer ${token}`;
+      api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
       setData({ user, token });
     } catch (err) {
       if (err.response) alert(err.response.data.message);
@@ -34,7 +34,7 @@ export function AuthProvider({ children }) {
     const token = localStorage.getItem('@rocketnotes:token');
 
     if(user && token) {
-      api.defaults.headers.authorization = `Bearer ${token}`;
+      api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
       setData({ user: JSON.parse(user), token });
     }
   }, []);
