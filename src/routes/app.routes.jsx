5efdf58fe9096833d4c5fceeab4912c295602128
apaolaoliveira <paolaoliveira.dev@gmail.com>
2024-10-16 23:45:02 -0300

    feat: handle unavailable routes

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
  import { NewNote } from '../pages/NewNote';
  import { Home } from '../pages/Home';
@@ -12,6 +12,7 @@ import { Routes, Route } from 'react-router-dom';
       <Route path="/new" element={<NewNote />} />
       <Route path="/profile" element={<Profile />} />
       <Route path="/details/:id" element={<Details />} />
+      <Route path="*" element={<Navigate to="/"/>}/>
     </Routes>
   )
  }
